fec22b4226025b8e5efc2cbaeda092bac7109f38
timothy tayson <timmy@innovativesupps.com>
2018-12-28 23:13:11 -0600

    generate question model via artisan model generator

--- app/User.php
+++ app/User.php
@@ -27,4 +27,18 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function questions(){
+        return $this->hasMany(Question::class);
+    }
+
+
+
+
+
+
+
+
+
+
 }
