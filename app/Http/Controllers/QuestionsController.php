ff4d30730cc41632b66d495284f8da68f8a8ecaf
timothy tayson <timmy@innovativesupps.com>
2018-12-29 19:54:14 -0600

    Implemnt authorizations using Gates

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -69,7 +69,11 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
+        if (\Gate::denies('update-question', $question)){
+            abort(403, "Access Denied");
+        }
         return view('questions.edit', compact('question'));
+
     }
 
     /**
@@ -81,6 +85,9 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        if (\Gate::denies('update-question', $question)){
+            abort(403, "Access Denied");
+        }
         $question->update($request->only('title', 'body'));
 
         return redirect()->route('questions.index')->with('Success', 'Your question has been updated');
@@ -94,6 +101,9 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        if (\Gate::denies('delete-question', $question)){
+            abort(403, "Access Denied");
+        }
         $question->delete();
 
         return redirect('/questions')->with('Success', 'Your question has been deleted');
