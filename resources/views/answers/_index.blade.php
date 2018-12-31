9aba1687a7e06a0073ca5e6dae7a52480059f3a7
tim <timmy@innovativesupps.com>
2018-12-31 08:18:10 -0600

    this was a test for if i can use the projext as normal at work since it was built at home

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -3,7 +3,7 @@
         <div class="card">
             <div class="card-body">
                 <div class="card-title">
-                    <h2>{{$answersCount . " " . str_plural('Answer', $answersCount)}}</h2>
+                    <h2>{{$answersCount . " " . str_plural('Answer', $answersCount)}} WAFFLES</h2>
                 </div>
                 <hr>
                 @include('layouts._messages')
